1d0e923b9299b9cca7dbfadbea14e8894a355101
Eliab E <eliaberango@gmail.com>
2024-01-10 12:39:21 -0800

    Add comment

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -41,14 +41,15 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Redact the message of LogRecord instance
+        """
+        redact the message of LogRecord instance
         Args:
-            record (logging.LogRecord): LogRecord instance containing message
+        record (logging.LogRecord): LogRecord instance containing message
         Return:
             formatted string
         """
