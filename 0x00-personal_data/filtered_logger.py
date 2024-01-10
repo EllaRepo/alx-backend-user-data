f17207f322a3a8cd8beea78a0b52d7a373d3dd47
Eliab E <eliaberango@gmail.com>
2024-01-10 13:13:22 -0800

    Refactore get_db method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -82,10 +82,10 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
     host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    conn = mysql.connector.connect(user=user,
-                                   password=passwd,
-                                   host=host,
-                                   database=db_name)
+    conn = mysql.connector.connection.MySQLConnection(user=user,
+                                                      password=passwd,
+                                                      host=host,
+                                                      database=db_name)
     return conn
 
 
