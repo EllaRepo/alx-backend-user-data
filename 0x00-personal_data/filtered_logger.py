074b2261d8b34f4bfcf766b6439f2509a6a99b54
Eliab E <eliaberango@gmail.com>
2024-01-10 12:35:32 -0800

    Refactored

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -42,8 +42,6 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields):
-        """Initialization
-        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
