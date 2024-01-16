ff9157f1bd9feb11497ec8b9c4da07170efde843
Eliab E <eliaberango@gmail.com>
2024-01-16 05:57:05 -0800

    Add Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -32,3 +32,13 @@ class BasicAuth(Auth):
             return de
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        if decoded_base64_authorization_header is None or\
+                not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(':')
+        return email, password
