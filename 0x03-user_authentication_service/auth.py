d69b4a66bc3c00b2edd19ebad0b6c95a4ceb51bd
Eliab E <eliaberango@gmail.com>
2024-01-24 11:29:27 -0800

    Add Find user by session ID support

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,3 +69,14 @@ class Auth:
                 return session_id
         except Exception:
             pass
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Gets user by session_id
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except Exception:
+            return None
